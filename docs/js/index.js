44dedab56b2e07563a6f579a8718ecb2b056d987
jianfei <326214104@qq.com>
2017-01-05 17:24:36 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,11 +1,34 @@
 xmlplus("xp", function (xp, $_, t) {
     $_().imports({
         Index: {
-            css: "#text { color: red; }",
-            xml: "<h1 id='text'>hello, world</h1>",
+            xml: "<AJAX id='ajax' type='GET'/>",
             fun: function( sys, items, opts ) {
-                sys.text.css("font-size", "28px");
+				sys.ajax.on("success", function( e, data ) {
+					console.log(data);
+				});
+                items.ajax({url: "/02-naming"});
             }
-        }
+        },
+		AJAX: {
+			xml: "<void id='ajax'/>",
+			opt: { url: "http://xmlplus.net/", type: "POST", data: null, timeout: 5000, async: true },
+			fun: function ( sys, items, opts ) {
+				return function ( options_ ) { 
+					var xhr = new XMLHttpRequest,
+						options = xp.extend({}, opts, options_);
+					xhr.timeout = options.timeout;
+					xhr.ontimeout = xhr.onerror = function (event) {
+						sys.ajax.trigger(event.type, event);
+					};
+					xhr.onload = function (event) {
+						if ( xhr.status != 200 )
+							return sys.ajax.trigger("error", event);
+						sys.ajax.trigger("success", [xhr.responseText], false);
+					};
+					xhr.open(options.type, opts.url + options.url, options.async);
+					xhr.send(options.data && JSON.stringify(options.data));
+				};
+			}
+		}
     });
 });
