42a145b6742a208131044e2a29fc88f6b6f2fd7e
jianfei <326214104@qq.com>
2017-01-05 19:37:19 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -7,7 +7,7 @@ xmlplus("xp", function (xp, $_, t) {
 				  </div>"
         },
 		Banner: {
-			css: "#banner { background: #222; color: #fafafa; position: fixed; top: 0; height: 50px; box-shadow: 0 0 5px rgba(0,0,0,0.5); width: 100%; z-index: 100; }"
+			css: "#banner { background: #222; color: #fafafa; position: fixed; top: 0; height: 50px; box-shadow: 0 0 5px rgba(0,0,0,0.5); width: 100%; z-index: 100; }",
 			xml: "<div id='banner'/>"
 		},
 		Docs: {
