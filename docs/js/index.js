0661c8f638f059083a5364f1dc303ef9f133366d
jianfei <326214104@qq.com>
2017-01-05 22:11:16 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -23,9 +23,10 @@ xmlplus("xp", function (xp, $_, t) {
 			fun: function( sys, items, opts ) {
 				sys.ajax.on("success", function( e, data ) {
 					/\<body\>((.|\r|\n)*)\<\/body\>/g.test(data);
-					sys.content.elem().innerHTML = RegExp.$1;
-					var a = xp.parseXML(RegExp.$1);
-					console.log(a);
+					sys.content.append(RegExp.$1);
+					//sys.content.elem().innerHTML = RegExp.$1;
+					//var a = xp.parseXML(RegExp.$1);
+					//console.log(a);
 				});
                 items.ajax({url: "/02-naming"});
 			}
