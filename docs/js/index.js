ee6f6880b2c46465d96856d6233e515fbb1b08eb
jianfei <326214104@qq.com>
2017-01-05 22:05:10 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -24,7 +24,7 @@ xmlplus("xp", function (xp, $_, t) {
 				sys.ajax.on("success", function( e, data ) {
 					/\<body\>((.|\r|\n)*)\<\/body\>/g.test(data);
 					sys.content.elem().innerHTML = RegExp.$1;
-					sys.content.elem().lastChild.className = "";
+					sys.content.elem().lastChild.setAttribute("class", "");
 				});
                 items.ajax({url: "/02-naming"});
 			}
