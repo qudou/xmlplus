24ffdbe5d4cb1daaaa1cebeaa57fcca198cd2878
jianfei <326214104@qq.com>
2017-01-05 18:30:55 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,15 +3,39 @@ xmlplus("xp", function (xp, $_, t) {
         Index: {
             xml: "<div id='top'>\
 					<AJAX id='ajax' type='GET'/>\
-					<div id='test'/>\
+					<Docs id='test'/>\
 				  </div>",
             fun: function( sys, items, opts ) {
 				sys.ajax.on("success", function( e, data ) {
 					/\<body\>((.|\r|\n)*)\<\/body\>/g.test(data);
-					sys.test.elem().innerHTML = RegExp.$1;
+					items.test.elem().innerHTML = RegExp.$1;
 				});
                 items.ajax({url: "/02-naming"});
             }
+        },
+		Docs: {
+			css: "@media (max-width: 640px) { #docs { padding-bottom: 0; } }\
+				  @media (max-width: 960px) { #docs { padding-left: 10px; padding-right: 10px; }}\
+				  #docs { padding-top: 20px; padding-bottom: 80px; }\
+				  #docs { max-width: 960px; margin-left: auto; margin-right: auto; padding-left: 20px; padding-right: 20px; }",
+			xml: "<section id='docs' xmlns:i='.'>\
+					<i:Content id='content'/>\
+					<i:Nav id='nav'/>\
+				  </section>",
+			fun: function( sys, items, opts ) {
+				return sys.content;
+			}
+		},
+		Nav: {
+			css: "@media (max-width: 640px) { #nav { width: 100%; padding-bottom: 40px; } }\
+				  #nav { color: #2d2d2d; font-size: 14px; float: left; width: 210px; }",
+			xml: "<div id='nav'/>"
+		},
+		Content: {
+			css: "@media (max-width: 640px) { #content { width: 100%; } } \
+				  @media (max-width: 960px) { #content { width: calc(100% - 240px); max-width: 100%; } }\
+				  #content { float: right; width: 100%; max-width: 650px; }",
+            xml: "<div id='content'/>"
         },
 		AJAX: {
 			xml: "<void id='ajax'/>",
