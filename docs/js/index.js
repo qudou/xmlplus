85a89e109c80e65693d6a2c80fcc18f57cda95ce
jianfei <326214104@qq.com>
2017-01-06 19:07:08 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -44,9 +44,11 @@ xmlplus("xp", function (xp, $_, t) {
                  </main>"
         },
         Docs: {
-            xml: "<div class='container bs-docs-container' xmlns:i='docs'>\
+			css: "#docs { position: relative; }",
+            xml: "<div id='docs' class='container bs-docs-container' xmlns:i='docs'>\
                     <div class='row'>\
                         <i:Content id='content' class='col-md-9'/>\
+						<i:Overlay id='overlay'/>\
                         <div class='col-md-3'>\
                             <div class='bs-docs-sidebar hidden-print hidden-xs hidden-sm affix-top'><i:Navigator id='nav'/></div>\
                         </div>\
@@ -55,9 +57,13 @@ xmlplus("xp", function (xp, $_, t) {
                   </div>",
             fun: function( sys, items, opts ) {
                 sys.nav.on("change", function (e, target) {
+					sys.overlay.show();
                     items.ajax({url: target});
                 });
-                sys.ajax.on("success", items.content.val);
+                sys.ajax.on("success", function(e, data) {
+					sys.overlay.hide();
+					items.content.val(e, data);
+				});
             }
         }
     });
@@ -150,7 +156,15 @@ xmlplus("xp", function (xp, $_, t) {
                 }
                 return { val: setValue };
             }
-        }
+        },
+		Overlay: {
+			css: "#overlay{display:none;position:absolute;left:0;top:0;width:100%;height:100%;z-index:13000;-webkit-transition-duration:.4s;transition-duration:.4s}",
+			xml: "<div id=\"overlay\"> <Loader/> </div>"
+		},
+		Loader: {
+			css: "#preloader { position: absolute; left: 50%; top: 50%; padding: 8px; margin-left: -25px; margin-top: -25px; background: rgba(61, 106, 162, 0.8); z-index: 13500; border-radius: 5px; } #spinner { display: block; width: 34px; height: 34px; background-position: 50%; background-size: 100%; background-repeat: no-repeat; -webkit-animation: $spin 1s steps(12, end) infinite; animation: $spin 1s steps(12, end) infinite; background-image: url(\"data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D'0%200%20120%20120'%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20xmlns%3Axlink%3D'http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink'%3E%3Cdefs%3E%3Cline%20id%3D'l'%20x1%3D'60'%20x2%3D'60'%20y1%3D'7'%20y2%3D'27'%20stroke%3D'%23fff'%20stroke-width%3D'11'%20stroke-linecap%3D'round'%2F%3E%3C%2Fdefs%3E%3Cg%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(30%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(60%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(90%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(120%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(150%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.37'%20transform%3D'rotate(180%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.46'%20transform%3D'rotate(210%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.56'%20transform%3D'rotate(240%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.66'%20transform%3D'rotate(270%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.75'%20transform%3D'rotate(300%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.85'%20transform%3D'rotate(330%2060%2C60)'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E\"); } @-webkit-keyframes $spin { 100% { -webkit-transform: rotate(360deg); } } @keyframes $spin { 100% { transform: rotate(360deg); } }",
+			xml: "<div id=\"preloader\"> <span id=\"spinner\"/> </div>"
+		}
     });
     $_("tools").imports({
         AJAX: {
