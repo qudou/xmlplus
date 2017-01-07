27b2400ac38a9f6d44496163ad2b63ba4a6b5045
jianfei <326214104@qq.com>
2017-01-07 13:36:37 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -66,10 +66,10 @@ xmlplus("xp", function (xp, $_, t) {
                 sys.ajax.on("success", function(e, data) {
 					sys.overlay.hide();
 					items.content.val(table[ptr] = data);
-					$(sys.content.elem()).scrollspy($(sys.content.elem()).data());
+					$(sys.nav.elem()).scrollspy("refresh");
 				});
 				console.log($(sys.content.elem()).data());
-				$(sys.content.elem()).scrollspy($(sys.content.elem()).data());
+				$(sys.nav.elem()).scrollspy($(sys.content.elem()).data());
             }
         }
     });
