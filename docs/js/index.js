1527a03eccba385f4eecaf8a05b533fe4e4038ed
jianfei <326214104@qq.com>
2017-01-07 13:58:43 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -66,8 +66,7 @@ xmlplus("xp", function (xp, $_, t) {
                 sys.ajax.on("success", function(e, data) {
 					sys.overlay.hide();
 					items.content.val(table[ptr] = data);
-					console.log($("#navigator").length);
-					//$(sys.content.elem()).scrollspy({target: "#navigator"});
+					$(sys.content.elem()).scrollspy({target: "#navigator", offset: 100});
 				});
             }
         }
