08cd601b81bd55fa734a4ff651b3dcba10cb98f8
jianfei <326214104@qq.com>
2017-01-07 15:48:45 +0800

    add

--- docs/js/index.js
+++ docs/js/index.js
@@ -45,28 +45,14 @@ xmlplus("xp", function (xp, $_, t) {
 			css: "#docs { position: relative; }",
             xml: "<div id='docs' class='container bs-docs-container' xmlns:i='docs'>\
                     <div class='row'>\
-                        <i:Content id='content' class='col-md-9'/>\
-						<i:Overlay id='overlay'/>\
+                        <div id='content' class='col-md-9'>\
+							<i:Article url='01-components-and-space'/>\
+						</div>\
                         <div class='col-md-3'>\
                             <div class='bs-docs-sidebar hidden-print hidden-xs hidden-sm affix-top'><i:Navigator id='nav'/></div>\
                         </div>\
                     </div>\
-                    <AJAX id='ajax' type='GET' xmlns='tools'/>\
-                  </div>",
-            fun: function( sys, items, opts ) {
-				var table = {}, ptr = null;
-                sys.nav.on("change", function (e, target) {
-					if ( ptr == target ) return;
-					if ( table[target] )
-						return items.content.val(table[ptr = target]);
-					sys.overlay.show();
-					items.ajax({url: ptr = target});
-                });
-                sys.ajax.on("success", function(e, data) {
-					sys.overlay.hide();
-					items.content.val(table[ptr] = data);
-				});
-            }
+                  </div>"
         }
     });
     $_("banner").imports({
@@ -116,66 +102,52 @@ xmlplus("xp", function (xp, $_, t) {
         Navigator: {
             css: "#nav { width: 100%; }\
                   .bs-docs-sidebar .nav>li>a { cursor: pointer; font-size: 14px; }",
-            xml: "<ul id='nav' class='nav bs-docs-sidenav' xmlns:i='nav'>\
-                    <li dt='01-components-and-space' id='first'><a>组件与空间</a></li>\
-                    <li dt='02-naming'><a>命名</a></li>\
-                    <li dt='03-abstract'><a>抽象</a></li>\
-                    <li dt='04-dynamic-interface'><a>动态接口</a></li>\
-                    <li dt='05-static-interface'><a>静态接口</a></li>\
-                    <li dt='06-parameter-mapping'><a>参数映射</a></li>\
-                    <i:Path dt='07-path'/>\
-                    <li dt='08-inheritting'><a>继承</a></li>\
-                    <li dt='09-searching'><a>检索</a></li>\
-                    <li dt='10-nesting'><a>嵌套</a></li>\
-                    <li dt='11-life-circle'><a>生命周期</a></li>\
-                    <li dt='12-events-and-communication'><a>事件与通信</a></li>\
-                    <li dt='13-message-and-communication'><a>消息与通信</a></li>\
-                    <li dt='14-sharing'><a>共享与通信</a></li>\
-                    <li dt='15-lazy-instantiation'><a>延迟实例化</a></li>\
+            xml: "<ul id='nav' class='nav bs-docs-sidenav'>\
+                    <li id='01-components-and-space'><a>组件与空间</a></li>\
+                    <li id='02-naming'><a>命名</a></li>\
+                    <li id='03-abstract'><a>抽象</a></li>\
+                    <li id='04-dynamic-interface'><a>动态接口</a></li>\
+                    <li id='05-static-interface'><a>静态接口</a></li>\
+                    <li id='06-parameter-mapping'><a>参数映射</a></li>\
+                    <li id='07-path'><a>路径</a></li>\
+                    <li id='08-inheritting'><a>继承</a></li>\
+                    <li id='09-searching'><a>检索</a></li>\
+                    <li id='10-nesting'><a>嵌套</a></li>\
+                    <li id='11-life-circle'><a>生命周期</a></li>\
+                    <li id='12-events-and-communication'><a>事件与通信</a></li>\
+                    <li id='13-message-and-communication'><a>消息与通信</a></li>\
+                    <li id='14-sharing'><a>共享与通信</a></li>\
+                    <li id='15-lazy-instantiation'><a>延迟实例化</a></li>\
                  </ul>",
             fun: function( sys, items, opts ) {
-                setTimeout(function() {
-                    sys.first.trigger("click");
-                });
-                sys.nav.on("click", "./*[@dt]", function (e) {
-                    e.stopPropagation();
-                    this.trigger("change", "docs/" + this.attr("dt"));
-                });
+				var template = "<li class='nav'><a href='%href'>%text</a></li>";
+                this.watch("article-ready", function ( e, dt ) {
+					sys[dt.url].first().attr("href", "#" + dt.h1.getAttribute("id"));
+					var ul = "<ul class='nav'>";
+					dt.h2s.forEach(function(h2) {
+						ul += template.replace('%href', '#' + h2.getAttribute("id")).replace('%text', h2.innerHTML);
+					});
+					sys[dt.url].append(ul += "</ul>");
+				});
             }
         },
-        Content: {
-            xml: "<div id='content' class='col-md-9'/>",
-            fun: function( sys, items, opts ) {
-                var content = sys.content.elem();
-                function setValue( value ) {
-                    var regexp = /\<body\>((.|\r|\n)*)\<\/body\>/g;
+		Article: {
+			xml: "<div id='article' class='bs-docs-section'/>",
+			fun: fun: function( sys, items, opts ) {
+				var str, msg, 
+					article = sys.article.elem(),
+					ajax = this.append("/tools/AJAX"),
+					regexp = /body\>((.|\r|\n)*)\<\/body/g;
+				ajax.once("success", function (e, data) {
                     regexp.test(value);
-                    content.innerHTML = RegExp.$1.trim();
-                    content.lastChild.removeAttribute("class");
-                    content.lastChild.setAttribute("class", "bs-docs-section");
-					window.scrollTo(window.scrollX,0);
-                }
-                return { val: setValue };
-            }
-        },
-		Overlay: {
-			css: "#overlay{display:none;position:fixed;left:0;top:0;width:100%;height:100%;z-index:13000;-webkit-transition-duration:.4s;transition-duration:.4s}",
-			xml: "<div id=\"overlay\"> <Loader/> </div>"
-		},
-		Loader: {
-			css: "#preloader { position: absolute; left: 50%; top: 50%; padding: 8px; margin-left: -25px; margin-top: -25px; background: rgba(61, 106, 162, 0.8); z-index: 13500; border-radius: 5px; } #spinner { display: block; width: 34px; height: 34px; background-position: 50%; background-size: 100%; background-repeat: no-repeat; -webkit-animation: $spin 1s steps(12, end) infinite; animation: $spin 1s steps(12, end) infinite; background-image: url(\"data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D'0%200%20120%20120'%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20xmlns%3Axlink%3D'http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink'%3E%3Cdefs%3E%3Cline%20id%3D'l'%20x1%3D'60'%20x2%3D'60'%20y1%3D'7'%20y2%3D'27'%20stroke%3D'%23fff'%20stroke-width%3D'11'%20stroke-linecap%3D'round'%2F%3E%3C%2Fdefs%3E%3Cg%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(30%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(60%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(90%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(120%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.27'%20transform%3D'rotate(150%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.37'%20transform%3D'rotate(180%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.46'%20transform%3D'rotate(210%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.56'%20transform%3D'rotate(240%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.66'%20transform%3D'rotate(270%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.75'%20transform%3D'rotate(300%2060%2C60)'%2F%3E%3Cuse%20xlink%3Ahref%3D'%23l'%20opacity%3D'.85'%20transform%3D'rotate(330%2060%2C60)'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E\"); } @-webkit-keyframes $spin { 100% { -webkit-transform: rotate(360deg); } } @keyframes $spin { 100% { transform: rotate(360deg); } }",
-			xml: "<div id=\"preloader\"> <span id=\"spinner\"/> </div>"
-		}
-    });
-	$_("docs/nav").imports({
-		Path: {
-			xml: "<li>\
-				 	<a href='#path'>路径</a>\
-				 	<ul class='nav'>\
-				 		<li><a href='#absolute-path'>绝对路径</a></li>\
-				 		<li><a href='#relative-path'>相对路径</a></li>\
-				 	</ul>\
-				  </li>"
+                    str = RegExp.$1.trim();
+					article.innerHTML = str.substr(5, str.length - 11);
+					(msg = [].slice.call(article.querySelectorAll("h1,h2"))).forEach(function(item) {
+						item.setAttribute("id", xp.guid());
+					});
+					this.notify("article-ready", {url: opts.url, h1: msg[0], h2s: msg.slice(1)});
+				}).value()({type: 'GET', url: opts.url});
+			}
 		}
 	});
     $_("tools").imports({
