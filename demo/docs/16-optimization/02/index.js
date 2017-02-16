de84072eea10a1354d70b4b90d9b2063611d932b
jianfei <326214104@qq.com>
2017-02-16 21:13:54 +0800

    commit

--- demo/docs/16-optimization/02/index.js
+++ demo/docs/16-optimization/02/index.js
@@ -1,10 +1,10 @@
 xmlplus("xp", function (xp, $_, t) {
-	$_().imports({
-		Index: {
-			xml: "<h1 id='index'>hello,world</h1>",
-			fun: function (sys, items, opts) {
-				console.log(sys.index.width());
-			}
-		}
-	});
+    $_().imports({
+        Index: {
+            xml: "<h1 id='index'>hello,world</h1>",
+            fun: function (sys, items, opts) {
+                console.log(sys.index.width());
+            }
+        }
+    });
 });
--- demo/docs/16-optimization/03/index.js
+++ demo/docs/16-optimization/03/index.js
@@ -1,11 +1,11 @@
 xmlplus("xp", function (xp, $_, t) {
-	$_().imports({
-		Index: {
-			xml: "<h1 id='index'>hello,world</h1>",
-			map: { nofragment: true },
-			fun: function (sys, items, opts) {
-				console.log(sys.index.width());
-			}
-		}
-	});
+    $_().imports({
+        Index: {
+            xml: "<h1 id='index'>hello,world</h1>",
+            map: { nofragment: true },
+            fun: function (sys, items, opts) {
+                console.log(sys.index.width());
+            }
+        }
+    });
 });
