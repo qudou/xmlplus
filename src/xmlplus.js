f014b7d06a1397f776a41776b6ee1d70b2d659d4
jianfei <326214104@qq.com>
2017-04-09 11:18:35 +0800

    commit

--- src/xmlplus.js
+++ src/xmlplus.js
@@ -1,7 +1,7 @@
 /*!
- * xmlplus.js v1.5.7
- * https://xmlplus.net
- * (c) 2009-2016 qudou
+ * xmlplus.js v1.5.8
+ * http://xmlplus.net
+ * (c) 2009-2017 qudou
  * Released under the MIT license
  */
  (function (isInBrowser, undefined) {
